f0ef3f6bcc285410d66d74f0d48fa6f652b1ed21
majianwei <jianwei.ma@hand-china.com>
2023-06-15 16:05:27 +0800

    [ADD] 添加根据条件查询用户

--- src/main/java/com/time/blog/domain/entity/User.java
+++ src/main/java/com/time/blog/domain/entity/User.java
@@ -3,11 +3,13 @@ package com.time.blog.domain.entity;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.io.Serializable;
 import java.util.Date;
@@ -48,9 +50,13 @@ public class User implements Serializable {
     private String deleteFlag;
 
     @ApiModelProperty("创建日期")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date creationDate;
 
     @ApiModelProperty("最后更新时间")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private Date lastUpdateTime;
 
 }
